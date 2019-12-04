9161997e7310486cb6b7c4f4903a92394b485414
Lukasz Fundament <lukf1993@gmail.com>
2019-12-04 17:47:28 +0100

    set rendering from state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import uid from 'uniqid';
 import Card from './components/Card';
 import Board from './components/Board';
+import bridge from './bridge.png'
 
-function App() {
+const App = () => {
+
+  const [sentence, setSentencs] = useState([]);
+  const [answer, setAnswer] = useState([]);
+
+  useEffect(() => {
+    setSentencs(
+      [
+        {name: "bridge.", className: "jsOne", id: uid()},
+        {name: "is", className: "jsTwo", id: uid()},
+        {name: "It", className: "jsThree", id: uid()},
+        {name: "a", className: "jsFour", id: uid()},
+      ]
+    )
+    setAnswer([
+      {name: "It", className: "jsThree"},
+      {name: "is", className: "jsTwo"},
+      {name: "a", className: "jsFour"},
+      {name: "bridge.", className: "jsOne"}
+    ])
+  }, [])
+
+
   return (
-    <div className='flexbox'>
-      <Board id="board-1" className="board">
-        <Card id="card-1" className="card board-3" draggable="true">
-          <p>Card one</p>
-        </Card>
-      </Board>
-      <Board id="board-2" className="board">
-        <Card id="card-2" className="card board-3" draggable="true">
-          <p>Card two</p>
-        </Card>
-      </Board>
-      <Board id="board-3" className="board" />
-      
-
-    </div>
-    
+    <>
+    <img className='img' src={bridge}></img>
+    <Board className='flexbox'>
+      <div className='flex'>
+      {answer.map(item => (
+        <Board id={item.className} className='board'/>
+      ))}
+      </div>
+      <Board id="board-1" className='board board__word'>
+        {sentence.map((item) => (
+          <Card 
+            id={item.id} 
+            className={`card ${item.className}`} 
+            draggable='true'>{item.name} 
+          </Card>
+        ))}
+        </Board>
+    </Board>
+    </>    
   );
 }
 
