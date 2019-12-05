13ff16713f3a961c9258e532dcaceb73a724afc3
Lukasz Fundament <lukf1993@gmail.com>
2019-12-05 09:28:50 +0100

    add styles from project

--- my-app/src/components/Board.js
+++ my-app/src/components/Board.js
@@ -7,7 +7,7 @@ function Board(props) {
         const card_id = e.dataTransfer.getData('card_id');
 
         const card = document.getElementById(card_id);
-        card.style.display = 'block';
+        card.style.display = 'flex';
 
         if(card.classList.contains(e.target.id)) {
             e.target.appendChild(card);
