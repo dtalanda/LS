4579953d17f90db2bf4fd1dcbaf93fcb1486a632
Lukasz Fundament <lukf1993@gmail.com>
2019-12-09 11:43:45 +0100

    fix routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className='menu'>
       <Link to={'/dragndrop'}><button className='menu__item'>Drag and Drop</button></Link>
-      <Link to={'/blockgame'}><button className='menu__item'>Block Game</button></Link>
+      <Link to={'/maze'}><button className='menu__item'>Block Game</button></Link>
       <Link to={'/memory'}><button className='menu__item'>Memory Game</button></Link>
     </div>    
   );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import App from './App';
 import DragnDrop from './page/DragnDrop'
 import Memory from './page/Memory';
-import BlockGame from './page/BlockGame';
+import Maze from './page/Maze';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
@@ -12,7 +12,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path='/blockgame' component={BlockGame} />
+            <Route path='/maze' component={Maze} />
             <Route path='/memory' component={Memory} />
             <Route path='/dragndrop' component={DragnDrop} />
             <Route path='/' component={App} />
