bfc414ee760c9486aa944cfb6a1d4af866385012
Lukasz Fundament <lukf1993@gmail.com>
2019-12-30 09:18:43 +0100

    create state for cloud and projector

--- my-app/src/page/Cloud.js
+++ my-app/src/page/Cloud.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CloudTitle from '../components/cloud/CloudTitle';
+import CloudBoard from '../components/cloud/CloudBoard';
+import block from '../icon/block.png';
+import train from '../icon/train.png';
+import pencil from '../icon/pencil.png';
+import leg from '../icon/leg.png';
+import window from '../icon/window.png';
+
 
 const Cloud = () => {
 
+    const [sentence, setSentence] = useState([
+        {sentence: "I've got blocks.", key: 1},
+        {sentence: "I've got a train.", key: 2},
+        {sentence: "It's a pencil.", key: 3},
+        {sentence: "It's my leg.", key: 4},
+        {sentence: "t's a window.", key: 5},
+    ])
+
+    const [img, setImg] = useState([
+        {img: block, key: 1},
+        {img: train, key: 2},
+        {img: pencil, key: 3},
+        {img: leg, key: 4},
+        {img: window, key: 5},
+
+
+    ])
+
 
     return (
-<h1>a</h1>
+        <>
+        <CloudTitle sentence={sentence} />
+        <CloudBoard img={img} />
+        </>
+
     )
 }
 
--- my-app/src/page/Projector.js
+++ my-app/src/page/Projector.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ProjectorBoard from '../components/projector/ProjectorBoard';
+import book from '../icon/book.png';
+import bed from '../icon/bed.png';
+import rainbow from '../icon/rainbow.png';
+import ball from '../icon/ball.png';
+import sea from '../icon/sea.png';
+import bridge from '../icon/bridge.png';
+
 
 const Projector = () => {
 
+    const [arr, setArr] = useState([]);
+
+    useEffect(() => {
+        setArr([
+            {source: 'Book', key: 1},
+            {source: bed, key: 3},
+            {source: 'Sea', key: 6},
+            {source: bridge, key: 2},
+            {source: rainbow, key: 4},
+            {source: 'Ball', key: 5},
+            {source: 'Rainbow', key: 4},
+            {source: 'Bed', key: 3},
+            {source: sea, key: 6},
+            {source: book, key: 1},
+            {source: 'Bridge', key: 2},
+            {source: ball, key: 5},
+        ])
+    }, [])
+
 
     return (
-<h1>a</h1>
+    <>
+        <ProjectorBoard arr={arr} />
+    </>
     )
 }
 
